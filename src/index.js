2040c1e5a172c43578a815f30951b23eea48683f
Adeebahmed333 <adeebahmed3337@gmail.com>
2023-12-01 15:50:06 +0530

    Configure Basic Sequelize Setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const SetupAndStartServer= async()=>
 {
   //createing the express object
   const app=express();
+  app.use(express.json());
   app.listen(PORT,()=>
   {
    console.log(`Server Started at PORT ${PORT} `)
