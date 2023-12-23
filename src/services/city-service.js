26c057b7f424c548a341964beaf882d297dd9ade
Adeebahmed333 <adeebahmed3337@gmail.com>
2023-12-23 15:27:20 +0530

    Added City Service

---
+++ src/services/city-service.js
@@ -0,0 +1,48 @@
+const { CityRepository } = require("../repository/index");
+
+class CityService {
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
+
+  async createCity(data)
+  {
+     try {
+        const city=await this.cityRepository.createCity(data);
+        return city;
+     } catch (error){
+        console.log("Something Went Wrong In Service Layer");
+     }
+  }
+
+  async deleteCity(cityid)
+  {
+    try {
+        const response=  await this.cityRepository.deleteCity(cityid);
+        return response;
+    } catch (error) {
+       console.log("Something Went Wrong In Service Layer");
+    }
+  }
+
+  async getCity(cityId,data)
+  {
+    try {
+        const city=await this.cityRepository.updateCity(cityid,data);
+        return city;
+    } catch (error) {
+       console.log("Something Went Wrong In Service Layer");
+    }
+  }
+  async updateCity(cityId)
+  {
+    try {
+        const city=await this.cityRepository.getCity(cityId);
+        return city;
+    } catch (error) {
+       console.log("Something Went Wrong In Service Layer");
+    }
+  }
+}
+
+module.exports = CityService;
